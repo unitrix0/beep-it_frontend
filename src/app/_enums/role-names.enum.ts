7fab032f2192ba3675bd5fda056e5bf2fc18f0a5
Marco Rebsamen <mrebsamen@unimatrix0.ch>
2019-10-14 21:59:51 +0200

    Add Identity Core

---
+++ src/app/_enums/role-names.enum.ts
@@ -0,0 +1,11 @@
+export enum RoleNames {
+  isOwner = 'IsOwner',
+  removeMember = 'RemoveMember',
+  inviteMember = 'InviteMember',
+  editArticleSettings = 'EditArticleSettings',
+  viewArticles = 'ViewArticles',
+  checkIn = 'CheckIn',
+  checkOut = 'CheckOut',
+  manageUsers = 'ManageUsers',
+  manageArticleBaseData = 'ManageArticleBaseData'
+}
