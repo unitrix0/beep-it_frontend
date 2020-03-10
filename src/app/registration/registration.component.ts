4a9516e15684bcd3bfc18bd5bd56d24240ef56a1
Marco Rebsamen <mrebsamen@unimatrix0.ch>
2020-03-10 20:19:09 +0100

    [BEEP-44] Validierung bei Registration form

--- src/app/registration/registration.component.ts
+++ src/app/registration/registration.component.ts
@@ -3,7 +3,6 @@ import {UserForRegistration} from '../_models/user-for-registration';
 import {AuthService} from '../_services/auth.service';
 import {UserForLogin} from '../_models/user-for-login';
 import {Router} from '@angular/router';
-import {HttpHeaders} from '@angular/common/http';
 import {environment} from '../../environments/environment';
 import {AlertifyService} from '../_services/alertify.service';
 
@@ -16,6 +15,7 @@ import {AlertifyService} from '../_services/alertify.service';
 export class RegistrationComponent implements OnInit {
   @Output() cancelRegistration = new EventEmitter();
   model: UserForRegistration = {displayName: '', email: '', password: '', username: ''};
+  confirmPw: string;
   private headers: string[];
 
   constructor(private authService: AuthService, private router: Router, private alertify: AlertifyService) {
@@ -29,14 +29,10 @@ export class RegistrationComponent implements OnInit {
   }
 
   register() {
-    let createdAt: string;
-
     this.authService.register(this.model).subscribe(response => {
       this.alertify.success('Willkommen bei Beep!');
-      createdAt = response.headers.get('location').replace(environment.apiUrl, '').toLowerCase();
-    }, error => {
-      this.alertify.error('Registrierung fehlgeschlagen: ' + error); // TODO Error Handling
-    }, () => {
+
+      const createdAt = response.headers.get('location').replace(environment.apiUrl, '').toLowerCase();
       const user: UserForLogin = new class implements UserForLogin {
         cameras: MediaDeviceInfo[];
         password: string;
@@ -44,11 +40,14 @@ export class RegistrationComponent implements OnInit {
       };
       user.username = this.model.username;
       user.password = this.model.password;
+      user.cameras = [];
 
       this.authService.login(user).subscribe(value => {
         this.alertify.success('Anmeldung erfolgreich!');
         this.router.navigate([createdAt]);
       });
+    }, error => {
+      this.alertify.error('Registrierung fehlgeschlagen: ' + error);
     });
   }
 }
