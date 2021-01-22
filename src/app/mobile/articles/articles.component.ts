ab25357f7af330aa12a4e9767aead57d0c6c56f1
Marco Rebsamen <mrebsamen@unimatrix0.ch>
2021-01-22 18:45:20 +0100

    Articles View with infinit scrolling

--- src/app/mobile/articles/articles.component.ts
+++ src/app/mobile/articles/articles.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {ArticlesService} from '../../_services/articles.service';
+import {Article} from '../../shared/_models/article';
+import {ArticlesFilter} from '../../shared/_models/articles-filter';
 
 @Component({
   selector: 'app-articles',
@@ -7,9 +10,34 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ArticlesComponent implements OnInit {
 
-  constructor() { }
+  articles: Article[];
+  filter: ArticlesFilter = {environmentId: '3', storeId: 0, isOpened: false, keepOnStock: false, isOnStock: false, nameOrEan: ''};
+  private maxPages: number;
+  private pagesLoaded = 1;
 
-  ngOnInit(): void {
+  constructor(public articlesService: ArticlesService) {
   }
 
+  ngOnInit(): void {
+    this.articlesService.getArticles(this.pagesLoaded, 15, this.filter)
+      .subscribe(result => {
+        this.maxPages = result.pagination.totalPages;
+        this.articles = result.content;
+      });
+  }
+
+  loadMore() {
+    if (this.pagesLoaded < this.maxPages) {
+      console.log(`${this.pagesLoaded} < ${this.maxPages} ${this.articles.length}`);
+      this.articlesService.getArticles((this.pagesLoaded + 1), 15, this.filter)
+        .subscribe(result => {
+          this.maxPages = result.pagination.totalPages;
+          this.articles = this.articles.concat(result.content);
+          this.pagesLoaded++;
+        }, error => {
+          console.log('error');
+          console.log(error);
+        });
+    }
+  }
 }
--- src/app/mobile/mobile.module.ts
+++ src/app/mobile/mobile.module.ts
@@ -8,14 +8,16 @@ import { NavComponent } from './nav/nav.component';
 import { ButtonBarComponent } from './button-bar/button-bar.component';
 import { ArticlesComponent } from './articles/articles.component';
 import { ShoppingListComponent } from './shopping-list/shopping-list.component';
+import {InfiniteScrollModule} from 'ngx-infinite-scroll';
 
 
 @NgModule({
   declarations: [ScanComponent, MainContainerComponent, NavComponent, ButtonBarComponent, ArticlesComponent, ShoppingListComponent],
-  imports: [
-    CommonModule,
-    MobileRoutingModule
-  ]
+    imports: [
+        CommonModule,
+        MobileRoutingModule,
+        InfiniteScrollModule
+    ]
 })
 export class MobileModule {
 
